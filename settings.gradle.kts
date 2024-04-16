eef7b29ad33bebfbcdf30a3966ec3d99258fddaa
Mohammed Khalid Hamid <khalid64927@gmail.com>
2024-04-16 23:17:58 +0800

    Chore: bumped gradle version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,5 @@
 /*
  * Copyright 2022 Mohammed Khalid Hamid. Use of this source code is governed by the Apache 2.0 license.
  */
-enableFeaturePreview("VERSION_CATALOGS")
 
 rootProject.name = "khalid-gradle-plugin"
